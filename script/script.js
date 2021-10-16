4e72924c03e6669192346166a90260510848db88
dima999 <dimadnepr1997@gmail.com>
2021-10-16 16:47:47 +0300

    'finished'

--- script/script.js
+++ script/script.js
@@ -10,4 +10,20 @@ menu.onclick = () =>{
 window.onscroll = () =>{
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
+
+    if(window.scrollY > 60){
+        document.querySelector('#scoll-top').classList.add('active');
+    }else{
+        document.querySelector('#scoll-top').classList.remove('active');
+    }
 }
+
+function loader(){
+    document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+    setInterval(loader, 3500);
+}
+
+window.onload = fadeOut();
